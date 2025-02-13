d655198de5abc1e6db28386783740e0b84987b33
tuananh-ng <158744840+tuananh-ng@users.noreply.github.com>
2025-02-13 09:33:41 +0100

    Update the screen when clicking numbers

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+updateScreenWhenClickNumbers();
+
 function printToScreen(string) {
     const screen = document.querySelector('.screen');
     screen.textContent = string;
+}
+
+function updateScreenWhenClickNumbers() {
+    const nums = document.querySelectorAll('.numbers button');
+    nums.forEach((num) => {
+        if (num.textContent === '.') return;
+        num.addEventListener('click', () => {
+            printToScreen(num.textContent);
+        });
+    });
 }
