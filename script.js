979de7168d3d2bb29515016dbfd2c9e0811f55f9
tuananh-ng <158744840+tuananh-ng@users.noreply.github.com>
2025-02-13 15:36:20 +0100

    Add functions to do basic calculations

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function updateScreenWhenClickNumbers() {
         });
     });
 }
+
+function add(x, y) {
+    return x + y;
+}
+
+function sub(x, y) {
+    return x - y;
+}
+
+function mul(x, y) {
+    return x * y;
+}
+
+function div(x, y) {
+    return x / y;
+}
