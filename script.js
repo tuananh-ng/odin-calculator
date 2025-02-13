50c7f68a866d954aac3a71bf4035cb9e6f9d86f1
tuananh-ng <158744840+tuananh-ng@users.noreply.github.com>
2025-02-13 16:03:01 +0100

    Change a function name

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const operators = {
     eq: eq,
 }
 
-updateScreenWhenClickNumbers();
+updateScreenWhenClickingNumbers();
 
 function printToScreen(string) {
     const screen = document.querySelector('.screen');
     screen.textContent = string;
 }
 
-function updateScreenWhenClickNumbers() {
+function updateScreenWhenClickingNumbers() {
     const nums = document.querySelectorAll('.numbers button');
     nums.forEach((num) => {
         if (num.textContent === '.') return;
